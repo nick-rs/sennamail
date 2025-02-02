d9a389058d16df8c40342bd37a43b3db73f3bf2a
nick-rs <nick0029920@gmail.com>
2025-02-02 00:05:17 +0000

    Remove full gradient on text, added copyright detail.

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,15 @@ export const viewport: Viewport = {
 
 export default function Home() {
     return (
+      // Main Content - Home Page
+      // Senna ©️ 2025 
         <>
         <div className='z-10 w-full max-w-xl px-5 xl:px-0 mx-auto mt-20 md:mt-32'>
         <h1
-          className="hover:animate-pulse bg-gradient-to-br from-black to-stone-500 dark:from-white dark:to-stone-400 text-transparent bg-clip-text text-4xl font-bold text-center md:text-6xl lg:text-7xl"
-          style={{ animationDelay: "0.15s", animationFillMode: "forwards", animationDuration: "1.5s", animationIterationCount: "1", animationTimingFunction: "ease-in-out" }}
+          className="hover:animate-pulse text-4xl font-bold text-center md:text-6xl lg:text-7xl"
+          style={{ animationDelay: "0.15s", animationFillMode: "forwards", animationDuration: "1.5s", animationIterationCount: "5", animationTimingFunction: "ease-in-out" }}
         >
-          senna.email
+          senna.<span className='bg-gradient-to-br from-black to-blue-400 dark:from-white dark:to-blue-400 text-transparent bg-clip-text'>mail</span>
         </h1>
         <p className='text-center mt-5 text-lg text-gray-400'>
             Encrypted no-log email service.
